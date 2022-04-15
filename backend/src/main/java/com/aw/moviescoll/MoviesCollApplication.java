7a05befa2c581a44b74d877962650592a0db93c0
Aleksander Wardzinski <aleks.wardzinski@gmail.com>
2022-04-15 20:01:06 +0200

    Init

---
+++ backend/src/main/java/com/aw/moviescoll/MoviesCollApplication.java
@@ -0,0 +1,13 @@
+package com.aw.moviescoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesCollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesCollApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/aw/moviescoll/MoviesCollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aw.moviescoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesCollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
