344df8bde4061fcbdf93159c718ddb95618422fd
Aleksander Wardzinski <aleks.wardzinski@gmail.com>
2022-04-21 23:23:45 +0200

    Fixes #24

--- backend/src/main/java/com/aw/moviescoll/MoviesCollApplication.java
+++ backend/src/main/java/com/aw/moviescoll/MoviesCollApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MoviesCollApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(MoviesCollApplication.class, args);
     }
 
