81216a9380940ad85a5cc530bc585da5aa2e05dd
Aleksander Wardzinski <aleks.wardzinski@gmail.com>
2022-05-09 15:28:43 +0200

    Adds TMDB attribution

--- frontend/src/app/layout/footer/footer.component.spec.ts
+++ frontend/src/app/layout/footer/footer.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { FooterComponent } from './footer.component';
+
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -8,9 +9,9 @@ describe('FooterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      declarations: [FooterComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -22,4 +23,15 @@ describe('FooterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a link to the author\'s repository', () => {
+    const links = fixture.debugElement.queryAll(By.css('a'));
+    expect(links.length).toBe(1);
+    expect(links[0].nativeElement.href).toContain('Eulodos');
+  })
+
+  it('should have the copyright symbol', () => {
+    const paragraph = fixture.debugElement.query(By.css('p')).nativeElement;
+    expect(paragraph.textContent).toContain('©');
+  })
 });
